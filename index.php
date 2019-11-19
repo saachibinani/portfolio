62e97cf7a857d60963e9373763409cb67c9ddfd4
saachibinani <saachibinani@gmail.com>
2019-11-19 17:28:52 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [Saachi Binani Period 2}</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Binani_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Binani_Projects.html">Projects.html</a></li></br>
+      <li><a href="#">Resume.html</a></li></br>
+      <li><a href="Binani_ContactMe.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
