e41aa9b2ab2d4f50da5a30cba662f51938d84143
saachibinani <57430084+saachibinani@users.noreply.github.com>
2020-04-16 18:34:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
       <!-- Please make sure to change all the #'s to the proper link names-->
       <li><a href="Binani_AboutMe.html">AboutMe.html</a></li></br>
       <li><a href="Binani_Projects.html">Projects.html</a></li></br>
-      <li><a href="#">Resume.html</a></li></br>
+      <li><a href="Binani_WorkExperience.html">WorkExperience.html</a></li></br>
       <li><a href="Binani_ContactMe.html">ContactMe.html</a></li></br>
     <ul>
   </body>
